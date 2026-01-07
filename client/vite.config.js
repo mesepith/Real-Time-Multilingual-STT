95b91f049ee19910c19246f141725f6196c55d86
mesepith <zahiralam29@gmail.com>
2026-01-07 15:12:34 +0530

    vite.config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 7058,
+    strictPort: true,
+    proxy: {
+      "/api": "http://localhost:7059",
+    },
+  },
+  build: {
+    outDir: "build", // you said you typically deploy a "build" folder
+  },
+});
